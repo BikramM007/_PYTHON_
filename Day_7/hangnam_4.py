32d37c07320393254ee761af47a43907ede11022
Bikram mahato <bikramisaking@gmail.com>
2023-02-01 10:30:24 +0530

    it time for death

--- Day_7/hangnam_4.py
+++ Day_7/hangnam_4.py
@@ -102,3 +102,5 @@ while not end_of_game:
 
     #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
     print(stages[lives])
+
+    print("hello world")
