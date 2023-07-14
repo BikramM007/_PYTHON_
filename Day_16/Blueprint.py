360e9f5461f695c39e4ec1f30b6e60c72b6b8d95
BikramM007 <mahatobikram720@gmail.com>
2023-07-14 23:39:07 +0530

    14-07-2023

---
+++ Day_16/Blueprint.py
@@ -0,0 +1,19 @@
+# from turtle import Turtle, Screen
+# bikram = Turtle()
+# print(bikram)
+# bikram.shape("turtle")
+# bikram.color("Red")
+# bikram.forward(1105)
+# bikram.right(45)
+
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("bikram",["100", "200", "300"])
+table.add_column("Ankur" ,["100", "200", "400"])
+print(table)
+
---
+++ Day_9/Bedding.py
@@ -0,0 +1,28 @@
+from click import clear
+from art import logo
+print(logo)
+
+bids = {}
+Bidding_finish = False
+
+def Find_Highest_biding(bidding_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+        
+while not Bidding_finish:
+    name = input("what is your name:")
+    price = int(input("What is your bid: $ "))
+    bids[name] = price
+    should_continue = input("Are they any others bedders 'yes' or 'no'.\n")
+    if should_continue == "no":
+        Bidding_finish = True
+        Find_Highest_biding(bids)
+    elif should_continue == "yes":
+        clear()
+    
